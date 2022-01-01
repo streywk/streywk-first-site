fca34f3c6a6f7ad6a3ccfa069a593f81c851b7f1
streywk <96955479+streywk@users.noreply.github.com>
2022-01-01 21:41:36 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,4 +8,5 @@ npm run dev
     <h6>Welcome to my first site!<h/6>
       <h3>Feel like it is your home!</h3>
         <p>Maybe you like <a href="https://github.com/">this</a></p>
-          
+   </head>
+</html>
