8ff82f62f50bf9f51cee157e0063010df27a3494
streywk <96955479+streywk@users.noreply.github.com>
2022-01-01 21:30:38 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
-<!DOCTYPE hmtl>
+<!DOCTYPE html>
   <html>
+  <body>
   <title>First site<title>
-  <head>
-  <h6>Welcome to my first site!<h6>
+    <h6>Welcome to my first site!<h6>
+      <h1>I owe you some money and I am not gonna give them to you!<h6>
+        <h2>HAHA!<h2>
+        
