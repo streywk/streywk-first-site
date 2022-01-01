75e601b9a68579e5f7241eef0ed2ce8381a8d096
streywk <96955479+streywk@users.noreply.github.com>
2022-01-01 21:47:39 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
-git clone https://github.com/netlify-templates/next-netlify-starter.git
-npm install
-npm run dev
-<!DOCTYPE html>
-  <html>
-  <head>
-  <title>First site</title>
-    <h6>Welcome to my first site!<h/6>
-      <h3>Feel like it is your home!</h3>
-        <p>Maybe you like <a href="https://github.com/">this</a></p>
-   </head>
-</html>
+<form name="contact" netlify>
+  <p>
+    <label>Name <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Email <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
